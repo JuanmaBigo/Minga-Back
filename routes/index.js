b08ccbf5b753f5011f8986b87afbb1041f73f82f
JuanmaBigo <juanmacuentatanlet@gmail.com>
2023-02-23 18:14:07 -0300

    Created Backend App

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Minga Backend Juanma' });
+});
+
+
+// en el enrutador principal voy a enrutar todos los recursos
+// es decir voy a llamar y configurar las rutas de usuarios, categorias, libros, etc
+// a traves del metodo .use() le indico al enrutador principal que utilice esas rutas con el endpoint correspondiente
+
+
+export default router;
