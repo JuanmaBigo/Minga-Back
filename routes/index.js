769c23ac54e482849bec7f02999defb3a5ad6459
JuanmaBigo <juanmacuentatanlet@gmail.com>
2023-03-01 18:59:43 -0300

    added Chapter model and chapters page

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 import userRouter from './users.js'
+import chapterRouter from './chapters.js'
 
 
 import express from 'express';
@@ -14,6 +15,7 @@ router.get('/', function (req, res, next) {
 // es decir voy a llamar y configurar las rutas de usuarios, categorias, libros, etc
 // a traves del metodo .use() le indico al enrutador principal que utilice esas rutas con el endpoint correspondiente
 router.use('/users',userRouter);
+router.use('/chapters',chapterRouter);
 
 
 export default router;
