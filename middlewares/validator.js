8d03f99a9a67192dbf1c27377ca1b37ab750e837
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2023-03-06 17:03:30 -0500

    M 07 (#14)
    * RUTA
    
    * pequeñas correcciones en el modelo
    
    * contoller para mostrat todas las categorias
    
    * middleware exists_tilte
    
    * alertas creadas para los campos de la creacion de un nuevo manga

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,7 +1,7 @@
 
 const validator = (schema) => [
     (req, res, next) => {
-        const validation = schema.validate(req.body, {abortEarly:false})
+        const validation = schema.validate(req.body, {abortEarly:true})
         //console.log(validation.error)
         //console.log(validation.error?.details) //acá es donde están todos los errores de validacion
         if (validation.error) { //si existe la propiedad error en la validacion: hay al menos un error en los datos del formulario
--- routes/mangas.js
+++ routes/mangas.js
@@ -2,6 +2,8 @@ import  express  from "express";
 import createcontroller from '../controllers/mangas/create.js'
 import showcontroller from '../controllers/categories/showAll.js'
 import exists_tittle from "../middlewares/exists_tittle.js";
+import validator from '../middlewares/validator.js'
+import schema from '../schemas/mangas.js'
 let router = express.Router()
 let { create } = createcontroller
 let { showAll } = showcontroller
@@ -9,8 +11,8 @@ let { showAll } = showcontroller
 
 
 router.get('/', showAll)
-router.post('/', exists_tittle, create)
 
+router.post('/', validator(schema), exists_tittle, create)
 
 
 export default router
