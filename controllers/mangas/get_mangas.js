b7381f0ee705973610cd881acd9152804da0b171
Johan Zuluaga <johan16zulu@gmail.com>
2023-03-07 21:40:09 -0500

    implementacion sort,pagination

--- controllers/mangas/get_mangas.js
+++ controllers/mangas/get_mangas.js
@@ -3,15 +3,31 @@ import Manga from "../../models/Manga.js"
 const controller = {
     get_mangas: async (req,res,next) => {
         let filter = {}
+        let pagination = {
+            page:1,
+            limit:6
+        }
         if(req.query.category){
             const categories = req.query.category.split(',');
             filter.category = { $in: categories };
+            pagination.limit=10
+            
         }
         if(req.query.title){
-            filter.title = new RegExp(req.query.title,'i') 
+            filter.title = new RegExp(req.query.title.trim(),'i') 
+            pagination.limit=10
+            
+        }
+        if(req.query.page){
+            pagination.page = req.query.page
         }
         try{
-            let get_mangas = await Manga.find(filter).select('title -_id')
+            let get_mangas = await Manga.find(filter)
+                .select('title -_id')
+                .sort({title:1})
+                .skip( pagination.page > 0 ? (pagination.page-1)*pagination.limit : 0 )
+                .limit(pagination.limit > 0 ? pagination.limit : 0 )
+
             return res
             .status(200)
             .json({ mangas: get_mangas })
