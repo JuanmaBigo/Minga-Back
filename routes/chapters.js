ad85b32888c633363e0fc34ba366cbc8d17696b6
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2023-03-08 09:59:57 -0500

    M 12 (#18)
    * sprint-2 
    
    * nuevas propiedades en user (#7)
    
    * creacion de modelo,controlador,ruta,middlewares (#9)
    
    * creacion de modelo,controlador,ruta,middlewares
    
    * creacion de auth token
    
    * arreglo de mensaje de los post y controllers-falta middleware de finds_id
    
    * added Chapter model and chapters page (#10)
    
    * RUTA (#11)
    
    * RUTA
    
    * pequeñas correcciones en el modelo
    
    * M 08 (#12)
    
    * added Chapter model and chapters page
    
    * added exists_order and next_order middlewares, added validator schema for chapter creation and create controller
    
    * organized code for better reading
    
    * changed create chapter route to /chapter-form
    
    * added passport authentication
    
    * completed merge
    
    * M 07 (#13)
    
    * RUTA
    
    * pequeñas correcciones en el modelo
    
    * contoller para mostrat todas las categorias
    
    * middleware exists_tilte
    
    * M 07 (#14)
    
    * RUTA
    
    * pequeñas correcciones en el modelo
    
    * contoller para mostrat todas las categorias
    
    * middleware exists_tilte
    
    * alertas creadas para los campos de la creacion de un nuevo manga
    
    * M 07 (#16)
    
    * RUTA
    
    * pequeñas correcciones en el modelo
    
    * contoller para mostrat todas las categorias
    
    * middleware exists_tilte
    
    * alertas creadas para los campos de la creacion de un nuevo manga
    
    * FIX
    
    * M 07 (#17)
    
    * RUTA
    
    * pequeñas correcciones en el modelo
    
    * contoller para mostrat todas las categorias
    
    * middleware exists_tilte
    
    * alertas creadas para los campos de la creacion de un nuevo manga
    
    * FIX
    
    * M 06 (#15) added middleword is_active
    
    ---------
    
    Co-authored-by: Juan Mateo Bigoritto <66440535+JuanmaBigo@users.noreply.github.com>
    Co-authored-by: eliasdiz <114044365+eliasdiz@users.noreply.github.com>
    
    * RUTA: controller  get_one.js// endpoint en el enrutador de chpaters
    
    * correccion de la ruta
    
    ---------
    
    Co-authored-by: JohanZ1611 <121687526+JohanZ1611@users.noreply.github.com>
    Co-authored-by: Juan Mateo Bigoritto <66440535+JuanmaBigo@users.noreply.github.com>

--- routes/chapters.js
+++ routes/chapters.js
@@ -7,12 +7,17 @@ import schema from '../schemas/chapters.js';
 import add_front_photo from '../middlewares/add_front_photo.js';
 import passport from '../middlewares/passport.js';
 
+import showOneChapterController from '../controllers/chapters/get_one.js'
+
 
 
 let router = express.Router();
 
+
+const { showOne } = showOneChapterController
 const { create } = controller;
 
+router.get('/:id',showOne)
 router.post('/', passport.authenticate('jwt', {session: false}), validator(schema), exists_order, next_order, add_front_photo, create);
 
 
