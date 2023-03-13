a8a18d8ef48ceb3ce54f1513a5e100e9e851b05f
JuanmaBigo <juanmacuentatanlet@gmail.com>
2023-03-13 18:42:30 -0300

    added Error handler middleware

--- controllers/mangas/get_one.js
+++ controllers/mangas/get_one.js
@@ -1,23 +1,22 @@
 import Manga from "../../models/Manga.js";
 import Company from "../../models/Company.js"
+import createError from 'http-errors'
 
 const controller = {
-    get_one: async (req, res) => {
+    get_one: async (req, res, next) => {
         try {
             let manga = await Manga.findById(req.params.id)
                 .select("title cover_photo description -_id")
                 .populate("company_id", "name -_id")
                 .populate("category_id", "name -_id")
-
-            return res
-                .status(200)
-                .json({ manga: manga })
-        } catch(error) {
-            console.log(error)
-            return res.status(400).json({
-                success: false,
-                message: 'manga does not exist',
-            })
+            if (manga) {
+                return res
+                    .status(200)
+                    .json({ manga: manga })
+            }
+            return next(createError(404, 'Manga does not exist'))
+        } catch (error) {
+            next(error)
         }
     }
 }
