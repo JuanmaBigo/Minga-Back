50f59b5e9d5e51639e5f306d441367d98ec5bb5c
Johan Zuluaga <johan16zulu@gmail.com>
2023-03-12 20:07:58 -0500

    creacion e implementacion tests

---
+++ test/index.test.js
@@ -0,0 +1,28 @@
+import app from '../app.js'
+import chai from 'chai'
+import request from 'supertest'
+
+const assert = chai.assert
+const expect = chai.expect
+
+describe('Prueba sobre/categories',() =>{
+
+    it('Get a /categories debe traerme 6',async() =>{
+        const response =await request(app).get('/api/mangas')
+        assert.equal(response.body.mangas.length,6)
+    })
+
+    it('Post a /user deberia crear un usuario',async() =>{
+        const user = {
+            name:'johanZ',
+            mail: "alejandro59@mh.com.ar",
+            password: "123456789",
+            photo: "https://i.postimg.cc/fyJsspq8/image.png"
+        }
+
+        const response = await request(app).post('/api/auth/signup').send(user)
+        assert.equal(response.body.success,true)
+    })
+
+
+})
