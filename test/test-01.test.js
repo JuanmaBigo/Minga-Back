196ec04ff428ecc1d05055aa93e8214163be12ba
JuanmaBigo <juanmacuentatanlet@gmail.com>
2023-03-17 11:03:51 -0300

    fixed  tests

--- test/test-01.test.js
+++ test/test-01.test.js
@@ -4,18 +4,20 @@ import request from 'supertest';
 
 const expect = chai.expect;
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MTBmYjMzYTdiM2RiOWEyYjJlMDcwMyIsImlhdCI6MTY3ODg5MTAyNywiZXhwIjoxNjc5NDk1ODI3fQ.2EPdswt4HQQ9P37ALde_DLxy2LCetXred6EIsnyAJVg';
+let manga = {
+    title: 'test-01',
+    cover_photo: 'https://img.freepik.com/foto-gratis/disparo-gran-angular-solo-arbol-que-crece-cielo-nublado-puesta-sol-rodeada-cesped_181624-22807.jpg',
+    description: 'descriptionn',
+    category_id: '640a14875120e75dcbb365e1'
+}
 
 describe('Test /mangas', () => {
     it('POST api/mangas verificar que cover_photo es una url', async () => {
+
+        expect(manga.cover_photo).include('https://');
         const response = await request(app).post('/api/mangas/')
             .set('Authorization', `Bearer ${token}`)
-            .send({
-                title: 'test-01',
-                cover_photo: 'https://img.freepik.com/foto-gratis/disparo-gran-angular-solo-arbol-que-crece-cielo-nublado-puesta-sol-rodeada-cesped_181624-22807.jpg',
-                description: 'descriptionn',
-                category_id: '640a14875120e75dcbb365e1'
-            });
-        expect(response.body.manga.cover_photo).include('https://');
+            .send(manga)
     })
 
     it('GET api/mangas verificar que se pasa token por headers', async () => {
@@ -25,7 +27,9 @@ describe('Test /mangas', () => {
     })
 
     it('POST api/mangas verificar que la respuesta devuelve "no autorizado" cuando no se pasa token', async () => {
-        await request(app).get(`/api/mangas`).expect(401);
+        await request(app).post(`/api/mangas`)
+            .send(manga)
+            .expect(401);
     })
 
     it('GET api/mangas verificar que la respuesta tiene alguna propiedad con el array de objetos (mangas)', async () => {
