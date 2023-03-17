427675f4266cc7a1ee898d926087d780a0496db1
Juan Mateo Bigoritto <66440535+JuanmaBigo@users.noreply.github.com>
2023-03-17 11:12:12 -0300

    T 01 (#36)
    * populate to obtain id
    
    * populate company_id
    
    * added Error handler middleware
    
    * test t-02
    
    * fix
    
    * begin test 01
    
    * added test for api/manga
    
    * fix
    
    * fix
    
    * fix
    
    * fixed  tests
    
    ---------
    
    Co-authored-by: Johan Zuluaga <johan16zulu@gmail.com>

--- test/test-01.test.js
+++ test/test-01.test.js
@@ -2,27 +2,38 @@ import app from '../app.js';
 import chai from 'chai';
 import request from 'supertest';
 
-const assert = chai.assert;
 const expect = chai.expect;
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MTBmYjMzYTdiM2RiOWEyYjJlMDcwMyIsImlhdCI6MTY3ODg5MTAyNywiZXhwIjoxNjc5NDk1ODI3fQ.2EPdswt4HQQ9P37ALde_DLxy2LCetXred6EIsnyAJVg'
-// REQ-TEST
-// POST api/mangas verificar que cover_photo es una url	
-// GET api/mangas verificar que se pasa token por headers	
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MTBmYjMzYTdiM2RiOWEyYjJlMDcwMyIsImlhdCI6MTY3ODg5MTAyNywiZXhwIjoxNjc5NDk1ODI3fQ.2EPdswt4HQQ9P37ALde_DLxy2LCetXred6EIsnyAJVg';
+let manga = {
+    title: 'test-01',
+    cover_photo: 'https://img.freepik.com/foto-gratis/disparo-gran-angular-solo-arbol-que-crece-cielo-nublado-puesta-sol-rodeada-cesped_181624-22807.jpg',
+    description: 'descriptionn',
+    category_id: '640a14875120e75dcbb365e1'
+}
 
 describe('Test /mangas', () => {
     it('POST api/mangas verificar que cover_photo es una url', async () => {
+        expect(manga.cover_photo).include('https://');
         const response = await request(app).post('/api/mangas/')
             .set('Authorization', `Bearer ${token}`)
-            .send({
-                title: 'test',
-                cover_photo: 'https://img.freepik.com/foto-gratis/disparo-gran-angular-solo-arbol-que-crece-cielo-nublado-puesta-sol-rodeada-cesped_181624-22807.jpg',
-                description: 'descriptionn',
-                company_id: 'test'
-            })
-            console.log(response.body)
-            // .expect(400)
-        // expect(response.body).to.have.property('success').that.is.a('boolean').that.is.false
+            .send(manga)
     })
 
+    it('GET api/mangas verificar que se pasa token por headers', async () => {
+        const response = await request(app).get(`/api/mangas/`)
+            .auth(token, { type: 'bearer' });
+        expect(response.request.header.Authorization).to.equal(`Bearer ${token}`);
+    })
 
+    it('POST api/mangas verificar que la respuesta devuelve "no autorizado" cuando no se pasa token', async () => {
+        await request(app).post(`/api/mangas`)
+            .send(manga)
+            .expect(401);
+    })
+
+    it('GET api/mangas verificar que la respuesta tiene alguna propiedad con el array de objetos (mangas)', async () => {
+        const response = await request(app).get(`/api/mangas`)
+            .auth(token, { type: 'bearer' });
+        expect(response.body).to.have.property('mangas');
+    })
 })
