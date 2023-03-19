adbd95047168532e60f74a7acdef307e8ab161c2
JuanmaBigo <juanmacuentatanlet@gmail.com>
2023-03-19 01:55:31 -0300

    updated middlewares find_id and is_active and added two new routes /:id to edit and delete a chapter with the corresponding controllers

--- routes/chapters.js
+++ routes/chapters.js
@@ -1,16 +1,18 @@
 import express from 'express';
-import controller from '../controllers/chapters/create.js';
+import passport from '../middlewares/passport.js';
 import exists_order from '../middlewares/exists_order.js';
 import next_order from '../middlewares/next_order.js';
 import validator from '../middlewares/validator.js';
 import schema from '../schemas/chapters.js';
 import add_front_photo from '../middlewares/add_front_photo.js';
-import passport from '../middlewares/passport.js';
-
-import get_chapters_controller from '../controllers/chapters/get_chapters.js';
-
+import controller from '../controllers/chapters/create.js';
+import getChaptersController from '../controllers/chapters/get_chapters.js';
 import showOneChapterController from '../controllers/chapters/get_one.js'
-
+import updateChapterController from '../controllers/chapters/update.js'
+import destroyChapterController from '../controllers/chapters/destroy.js'
+import find_id from '../middlewares/auth/find_id.js';
+import is_active from '../middlewares/authors/is_active.js';
+// import is_property_of from '../middlewares/authors/is_property_of.js';
 
 
 
@@ -19,11 +21,15 @@ let router = express.Router();
 
 const { showOne } = showOneChapterController
 const { create } = controller;
-const { get_chapters } = get_chapters_controller;
+const { get_chapters } = getChaptersController;
+const { update } = updateChapterController;
+const { destroy } = destroyChapterController;
 
+router.post('/', passport.authenticate('jwt', {session: false}), validator(schema), exists_order, next_order, add_front_photo, create);
 router.get('/', get_chapters)
 router.get('/:id',showOne)
-router.post('/', passport.authenticate('jwt', {session: false}), validator(schema), exists_order, next_order, add_front_photo, create);
+router.put('/:id', passport.authenticate('jwt', {session: false}), find_id, is_active, update);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), find_id, is_active, destroy);
 
 
 export default router;
