a6e7c371b694d1aa278a16c78f4be1ea57fbf007
Juan Mateo Bigoritto <66440535+JuanmaBigo@users.noreply.github.com>
2023-03-20 15:40:13 -0300

    M 14 (#45)
    * populate to obtain id
    
    * populate company_id
    
    * added Error handler middleware
    
    * test t-02
    
    * fix
    
    * begin test 01
    
    * added test for api/manga
    
    * fix
    
    * fix
    
    * fix
    
    * fixed  tests
    
    * updated middlewares find_id and is_active and added two new routes /:id to edit and delete a chapter with the corresponding controllers
    
    * added middlewares chapter_is_property_of and exists_order_update
    
    ---------
    
    Co-authored-by: Johan Zuluaga <johan16zulu@gmail.com>

--- middlewares/auth/find_id.js
+++ middlewares/auth/find_id.js
@@ -3,12 +3,11 @@ import Author from "../../models/Author.js";
 
 async function finds_id(req,res,next){
     const author = await Author.findOne({user_id: req.user.id})
-    console.log(author);
     if(author){
         req.body.author_id = author._id
-        // console.log({ user: req.user });
         return next()
     }
+
 
     return res.status(400).json({
         success: false,
