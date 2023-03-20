73b633ce6a52c0665153671ace87090baf066c10
JohanZ1611 <121687526+JohanZ1611@users.noreply.github.com>
2023-03-20 10:08:40 -0500

    M 15 (#42)
    * test t-02
    
    * test t-02
    
    * test t-02 finalizado
    
    * implementacion get y put del author
    
    * implementacion get y put del authors
    
    ---------
    
    Co-authored-by: Juan Mateo Bigoritto <66440535+JuanmaBigo@users.noreply.github.com>

--- middlewares/auth/find_id.js
+++ middlewares/auth/find_id.js
@@ -1,4 +1,5 @@
-import Author from "../../models/Author.js"
+import Author from "../../models/Author.js";
+
 
 async function finds_id(req,res,next){
     const author = await Author.findOne({user_id: req.user.id})
---
+++ schemas/authorUpdate.js
@@ -0,0 +1,66 @@
+import Joi from "joi-oid"
+import objectId from 'joi-objectid';
+Joi.objectId = objectId(Joi);
+
+const schema = Joi.object({
+    name: Joi
+        .string()
+        .required()
+        .min(2)
+        .max(30)
+        .messages({
+            'string.min': 'The title must be at least 2 characters long',
+            'string.max': 'The title must be 30 characters long max',
+            'string.empty': 'A title is required',
+            'any.required': 'A title is required'
+        }),
+    last_name: Joi
+        .string()
+        .min(2)
+        .max(30)
+        .messages({
+            'string.min': 'The title must be at least 2 characters long',
+            'string.max': 'The title must be 30 characters long max',
+            'string.empty': 'A title is required',
+            'any.required': 'A title is required'
+        }),
+    city: Joi
+        .string()
+        .min(5)
+        .max(30)
+        .messages({
+            'string.min': 'The title must be at least 5 characters long',
+            'string.max': 'The title must be 30 characters long max',
+            'string.empty': 'A title is required',
+            'any.required': 'A title is required'
+        }),
+    country: Joi
+        .string()
+        .min(5)
+        .max(30)
+        .messages({
+            'string.min': 'The title must be at least 5 characters long',
+            'string.max': 'The title must be 30 characters long max',
+            'string.empty': 'A title is required',
+            'any.required': 'A title is required'
+        }),
+    date: Joi
+        .date()
+        ,
+    photo: Joi
+        .string() 
+        .uri()
+        .messages(
+            {
+                'string.empty': 'The photo field cannot be empty',
+                'any.required': 'A photo is required',
+                'string.uri': 'A valid URL is necessary for the cover photo'
+            }),
+    user_id: Joi
+        .objectId()
+        .messages({
+            'invalid': 'user_id is not an objectId'
+        }),
+})
+
+export default schema
