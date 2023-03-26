c6a78beb04660dd2882cf737a8d50954a9bb8376
Johan Zuluaga <johan16zulu@gmail.com>
2023-03-26 16:22:11 -0500

    fix

--- controllers/mangas/get_one.js
+++ controllers/mangas/get_one.js
@@ -1,10 +1,6 @@
 import Manga from "../../models/Manga.js";
-<<<<<<< HEAD
-import createError from 'http-errors'
-=======
 import Company from "../../models/Company.js";
 import createError from 'http-errors';
->>>>>>> 33152d92a6c30f16c873c600480bbe7fffb0b274
 
 const controller = {
     get_one: async (req, res, next) => {
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -7,19 +7,9 @@ export function errorNotFound(req, res, next){
 export function errorHandler(err, req, res, next){
     console.error(err.stack)
     let body = {
-<<<<<<< HEAD
-        success:false,
-        status: err.status,
-        message: err.message
-    }
-    if (err.status === 404) {
-        body.manga = {}
-    }
-=======
         status: err.status,
         message: err.message
     }
     
->>>>>>> 33152d92a6c30f16c873c600480bbe7fffb0b274
     res.status(err.status || 500).json(body)
 }
