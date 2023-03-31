5dd5c6c4c9b28a8122c990bf4de72c805889afa1
Elias Jimenez Diz <eliasj.diz@gmail.com>
2023-03-31 16:02:20 -0500

    PUT: api/role/author/:id para modificar el rol de usuario comun a author

---
+++ controllers/admin/authors.js
@@ -0,0 +1,29 @@
+import Author from '../../models/Author.js'
+import User from '../../models/User.js';
+
+const controller = {
+
+    update_active: async (req,res,next) => {
+        try {
+            let author = await Author.findByIdAndUpdate(
+                req.params.id,
+                { active: true}
+            )
+            if ( author ){
+                await User.findByIdAndUpdate(
+                    { _id: author.user_id},
+                    { is_author: true}
+                )
+                return res
+                    .status(200)
+                    .json({
+                        message: 'Author is active now'
+                    })
+            }
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+export default controller
--- controllers/authors/update.js
+++ controllers/authors/update.js
@@ -21,5 +21,5 @@ const controller = {
             next(createError(400,error))
         }
     }
-   }
+}
 export default controller
